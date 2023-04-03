ce1b437a521e7cf6c3600815aa9b7a29eeb79823
Om Kumar Keshri <84616955+keshriom@users.noreply.github.com>
2023-04-03 10:10:09 +0530

    Time: 1 ms (94.93%), Space: 42.9 MB (28.90%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,6 +1,6 @@
 class Solution {
     //-------------Striver---------------
-//     1st approach where space compexity is more 
+//     1st approach where space compexity is more which is O(n)*O(n)
     private void recurPermute(int[] nums,List<List<Integer>> ans ,List<Integer> ds,boolean[] freq){//boolean[] freq is a map array
         if(ds.size()==nums.length){
             ans.add(new ArrayList<>(ds));
@@ -24,3 +24,31 @@ class Solution {
         return res;
 }
 }
+// ----Approach 2 which is efficient in space complexity which take O(n!)
+// private void recurPermute(int index, int[] nums, List < List < Integer >> ans) {
+//         if (index == nums.length) {
+//             // copy the ds to ans
+//             List < Integer > ds = new ArrayList < > ();
+//             for (int i = 0; i < nums.length; i++) {
+//                 ds.add(nums[i]);
+//             }
+//             ans.add(new ArrayList < > (ds));
+//             return;
+//         }
+//         for (int i = index; i < nums.length; i++) {
+//             swap(i, index, nums);
+//             recurPermute(index + 1, nums, ans);
+//             swap(i, index, nums);
+//         }
+//     }
+//     private void swap(int i, int j, int[] nums) {
+//         int t = nums[i];
+//         nums[i] = nums[j];
+//         nums[j] = t;
+//     }
+//     public List < List < Integer >> permute(int[] nums) {
+//         List < List < Integer >> ans = new ArrayList < > ();
+//         recurPermute(0, nums, ans);
+//         return ans;
+//     }
+// }
