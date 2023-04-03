b044d813088f98e480da2edf2ead3abed2899feb
Om Kumar Keshri <84616955+keshriom@users.noreply.github.com>
2023-04-03 09:47:01 +0530

    Time: 1 ms (94.93%), Space: 43 MB (28.90%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,17 +1,18 @@
 class Solution {
+    //-------------Striver---------------
 //     1st approach where space compexity is more 
-    private void recurPermute(int[] nums,List<List<Integer>> ans ,List<Integer> ds,boolean[] freq){
+    private void recurPermute(int[] nums,List<List<Integer>> ans ,List<Integer> ds,boolean[] freq){//boolean[] freq is a map array
         if(ds.size()==nums.length){
             ans.add(new ArrayList<>(ds));
             return;
         }
         for(int i=0;i<nums.length;i++){
-            if(!freq[i]){
-                freq[i]=true;
-                ds.add(nums[i]);
+            if(!freq[i]){//the array map is empty then 
+                freq[i]=true;//map it to true
+                ds.add(nums[i]);//add the value in datastructue
                 recurPermute(nums,ans,ds,freq);
-                ds.remove(ds.size()-1);
-                freq[i]=false;
+                ds.remove(ds.size()-1);//remove the top element from the ds
+                freq[i]=false;//and make it as false when moving back
         }
         }
     }
