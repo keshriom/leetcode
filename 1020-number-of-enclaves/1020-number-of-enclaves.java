9e0202d2d619605d08ff203a8ac568189f33ff25
Om Kumar Keshri <84616955+keshriom@users.noreply.github.com>
2023-04-07 14:52:54 +0530

    Time: 10 ms (61.79%), Space: 50.2 MB (91.21%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -0,0 +1,59 @@
+ class Pair{
+        int first;
+        int second;
+        public Pair(int first,int second){
+            this.first=first;
+            this.second=second;
+        }
+    }
+class Solution {
+    //--------striver---
+    public int numEnclaves(int[][] grid) {
+        //making a queue to store the index of 1's for the  given matrix
+        Queue<Pair> q=new LinkedList<Pair>();
+        int n=grid.length;//no. of rows
+        int m=grid[0].length;//no. of column
+        int vis[][]=new int[n][m];//creating a visited matrix
+        //visiting all the index of the grid
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                //first row , first col ,nth row , nth col
+                if(i==0 || j==0 || i==n-1 || j==m-1){
+                    if(grid[i][j]==1){
+                        q.add(new Pair(i,j));
+                        vis[i][j]=1;
+                    }
+                }
+            }
+        }
+        //direction of (i,j)
+        int deltarow[]={-1,0,+1,0};
+        int deltacol[]={0,+1,0,-1};
+        
+        while(!q.isEmpty()){
+            int row=q.peek().first;
+            int col=q.peek().second;
+            q.remove();
+            
+            //traverse all 4 directions
+            for(int i=0;i<4;i++){
+                int nrow= row + deltarow[i];
+                int ncol= col + deltacol[i];
+                //checking boundary conditon
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                    q.add(new Pair(nrow,ncol));
+                    vis[nrow][ncol]=1;
+                }
+            }
+        }
+        //count the no. of 1's present inside the grid 
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1 && vis[i][j]==0)
+                    count++;
+            }
+        }
+        return count;
+    }
+}
