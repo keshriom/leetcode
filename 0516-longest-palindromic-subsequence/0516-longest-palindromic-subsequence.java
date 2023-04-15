4d2741c221145411e120b2207a8bda1c3c04d24f
Om Kumar Keshri <84616955+keshriom@users.noreply.github.com>
2023-04-15 12:48:32 +0530

    Time: 96 ms (28.20%), Space: 51.9 MB (51.83%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -7,25 +7,25 @@ class Solution {
     public int lcs(String a ,String b){
         int m=a.length();
         int n=b.length();
-        int t[][]=new int[m+1][n+1];
+        int dp[][]=new int[m+1][n+1];
         
         for(int i=0;i<m+1;i++){
             for(int j=0;j<n+1;j++){
                 if(i==0 || j==0)//if length of string is zero
-                    t[i][j]=0;
+                    dp[i][j]=0;
                 //comparing the last character of the string 
                 else if(a.charAt(i-1)==b.charAt(j-1)){
-                    t[i][j]=1+t[i-1][j-1];
+                    dp[i][j]=1+dp[i-1][j-1];
                 }
                 //if last character is not equal then 1st time discard the last element of                      string a and then 2nd time discard the last element of string b and take                        maximum of the two value 
 
                 else {
-                    t[i][j]=Math.max(t[i][j-1],t[i-1][j]);
+                    dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
                 }
             }
         }
         
-        return t[m][n];
+        return dp[m][n];
         
     }
 }
