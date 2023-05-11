4e8a627278a4fa5adb862c38248407cf14f36eca
Om Kumar Keshri <84616955+keshriom@users.noreply.github.com>
2023-05-11 17:37:38 +0530

    Time: 6 ms (42.43%), Space: 42.7 MB (11.17%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.java
+++ 1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -7,7 +7,7 @@ class Solution {
         int[][] dp=new int[n1+1][n2+1];
         for(int i=0;i<=n1;i++){
             for(int j=0;j<=n2;j++){
-                //zeroth row and zeroth col should be zero always in the table
+                //if either of the of array is empty dp[i][j]==0
                 if(i==0 || j==0)
                     dp[i][j]=0;
                 
