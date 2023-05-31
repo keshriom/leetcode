dc2237767b181907a49025a47eea9306c50c6a4f
Om Kumar Keshri <84616955+keshriom@users.noreply.github.com>
2023-05-31 16:10:23 +0530

    Time: 71 ms (48.32%), Space: 43.5 MB (18.40%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+     //pepcoding----------
+        //tabulation method
+        int n=nums.length;
+        int[] dp=new int[n];
+        int overall_maximum=0;
+        for(int i=0;i<n;i++){
+            int max=0;
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    if(dp[j]>max){
+                        max=dp[j];
+                    }
+                }
+            }
+            dp[i]=max+1;
+            if(dp[i]>overall_maximum)
+                overall_maximum=dp[i];
+        }
+        return overall_maximum;
+    }
+}
