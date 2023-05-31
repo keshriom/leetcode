3257a75c8263191f36e5bc1403b4bc6c6fb08fff
Om Kumar Keshri <84616955+keshriom@users.noreply.github.com>
2023-05-31 17:09:50 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (9.20%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -9,7 +9,7 @@ class Solution {
         dp[1]=Math.max(nums[0],nums[1]);
          //storing the max_value into the dp[] array
         for(int i=2;i<nums.length;i++){
-            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);//create ur dp and then visualize it u can understand
         }
         return dp[nums.length-1];//return the last element
     }
