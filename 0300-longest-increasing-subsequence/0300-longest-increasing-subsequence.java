0d99e11b64bc91563fbfa9c9b704afa5f6520c35
Om Kumar Keshri <84616955+keshriom@users.noreply.github.com>
2023-05-31 16:10:55 +0530

    Time: 96 ms (20.75%), Space: 43.5 MB (20.62%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -5,7 +5,7 @@ class Solution {
         int n=nums.length;
         int[] dp=new int[n];
         int overall_maximum=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<dp.length;i++){//dp.length=n
             int max=0;
             for(int j=0;j<i;j++){
                 if(nums[j]<nums[i]){
